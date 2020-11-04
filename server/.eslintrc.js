7c48825e90a4e2d7645e1e617a75aad0c7f63abf
mhallaq <malhallaq@gmail.com>
2020-11-04 13:09:24 +0000

    changing ignore and eslint configuration

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,10 +1,25 @@
 module.exports = {
-	extends: ["@codeyourfuture/standard"],
-	parserOptions: {
-		ecmaVersion: 2017,
-	},
-	env: {
-		node: true,
-		es6: true,
-	},
+  extends: [
+    "@codeyourfuture/standard",
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:jsx-a11y/recommended",
+  ],
+  rules: {
+    "react/prop-types": 0,
+    "no-console": 1,
+  },
+  plugin: ["react", "import", "jsx-a11y"],
+  parserOptions: {
+    ecmaVersion: 2017,
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+  setting: {
+    react: {
+      version: "detect",
+    },
+  },
 };
