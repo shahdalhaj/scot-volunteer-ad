9d34dbceac9ffeba4f72612d9d5faec41978cae4
shahdalhaj <shahdadil2012@hotmail.com>
2020-11-18 16:42:26 +0000

    login

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "../styles/Home.css";
+import Login from "./Login";
 
 const Home = () => {
   return (
     <div>
       <header className="home-header">
-        <h1>Graduation Project skeleton</h1>
+        <Login />
       </header>
     </div>
   );
