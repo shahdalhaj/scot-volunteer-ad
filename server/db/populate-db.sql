c967475bc7843a9b2a27ec325e9d087f0865e71d
mhallaq <malhallaq@gmail.com>
2020-11-23 22:55:24 +0000

    removing auto-populated-column values

--- server/db/populate-db.sql
+++ server/db/populate-db.sql
@@ -4,12 +4,12 @@ INSERT INTO users (email, password) values ('admin@cyf.org', 'admin_password');
 INSERT INTO users (email, password) values ('user@cyf.org', 'user_password');
 
 
-INSERT INTO blocks (block_id, block_name) VALUES(1, 'Block 1');
-INSERT INTO blocks (block_id, block_name) VALUES(2, 'Block 2');
+INSERT INTO blocks (block_name) VALUES( 'Block 1');
+INSERT INTO blocks (block_name) VALUES( 'Block 2');
 
 
-INSERT INTO topics (topic_id, topic_name, document_name, document_link) VALUES(1, 'Beginners English', 'Beginners English', 'www.google.com');
-INSERT INTO topics (topic_id, topic_name, document_name, document_link) VALUES(2, 'Intermediate English', 'Intermediate English', 'www.bing.com');
+INSERT INTO topics ( topic_name, document_name, document_link) VALUES('eginners English', 'Beginners English', 'www.google.com');
+INSERT INTO topics ( topic_name, document_name, document_link) VALUES('Intermediate English', 'Intermediate English', 'www.bing.com');
 
 
 
@@ -19,10 +19,14 @@ INSERT INTO block_topics (block_id, topic_id) VALUES(2, 1);
 INSERT INTO block_topics (block_id, topic_id) VALUES(2, 2);
 
 
-INSERT INTO volunteer_topic (volunteer_topic_id, topic_id, volunteer_id) VALUES(1, 1, 1);
-INSERT INTO volunteer_topic (volunteer_topic_id, topic_id, volunteer_id) VALUES(2, 1, 2);
-INSERT INTO volunteer_topic (volunteer_topic_id, topic_id, volunteer_id) VALUES(3, 2, 1);
-INSERT INTO volunteer_topic (volunteer_topic_id, topic_id, volunteer_id) VALUES(4, 2, 2);
+INSERT INTO volunteers ( email) VALUES('Volunteer1@email.com');
+INSERT INTO volunteers ( email) VALUES('John@mail.com');
+
+
+INSERT INTO volunteer_topic ( topic_id, volunteer_id) VALUES(1, 1);
+INSERT INTO volunteer_topic ( topic_id, volunteer_id) VALUES(1, 2);
+INSERT INTO volunteer_topic ( topic_id, volunteer_id) VALUES(2, 1);
+INSERT INTO volunteer_topic ( topic_id, volunteer_id) VALUES(2, 2);
 
 
 INSERT INTO questions (topic_id, question_text) VALUES(1, 'What are the types of past tense?');
@@ -30,12 +34,10 @@ INSERT INTO questions (topic_id, question_text) VALUES(2, 'What are the types of
 
 
 
-INSERT INTO volunteers (volunteer_id, email) VALUES(1, 'Volunteer1@email.com');
-INSERT INTO volunteers (volunteer_id, email) VALUES(2, 'John@mail.com');
 
 
-INSERT INTO volunteer_answers (volunteer_topic_id, question_id, answers) VALUES(1, 1, 'Past simple, past continus');
-INSERT INTO volunteer_answers (volunteer_topic_id, question_id, answers) VALUES(1, 1, 'Simple Past Tense.Past Continuous Tense.
+INSERT INTO volunteer_answers (volunteer_topic_id, question_id, answer) VALUES(1, 1, 'Past simple, past continus');
+INSERT INTO volunteer_answers (volunteer_topic_id, question_id, answer) VALUES(2,1 , 'Simple Past Tense.Past Continuous Tense.
 Past Perfect Tense.
 Past Perfect Continuous Tense.
 ');
