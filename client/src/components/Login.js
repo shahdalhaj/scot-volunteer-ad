2fa36e83950db27fe8e124af9c0fabb6dba739ab
mhallaq <malhallaq@gmail.com>
2020-11-24 15:20:53 +0000

    Add Authentication

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { signApi } from "../api/auth";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -55,113 +56,149 @@ const useStyles = makeStyles(theme => ({
 function Login() {
   const classes = useStyles();
 
-  return (
-    <Grid container component="main" xs={12} sm={8} className={classes.root}>
-      <CssBaseline />
-      <Grid
-        item
-        xs={false}
-        sm={6}
-        md={5}
-        className={classes.image}
-        style={{ display: "flex", justifyContent: "flex-start" }}
-      >
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            flexDirection: "column",
-            height: "150px"
-          }}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const handleChange = event => {
+    if (event.target.name === "email") {
+      setEmail(event.target.value);
+    }
+    if (event.target.name === "password") {
+      setPassword(event.target.value);
+    }
+  };
+  const signIn = () => {
+    signApi(email, password)
+      .then(data => {
+        const token = data.token;
+        // console.log(token);
+
+        localStorage.setItem("token", token);
+        localStorage.setItem("user", JSON.stringify(data.user));
+        document.location.reload();
+      })
+      .catch(() => {
+        setError(true);
+      });
+  };
+
+  const token = localStorage.getItem("token");
+  if (token) {
+    return <div>You are already logged in.</div>;
+  } else
+    return (
+      <Grid container component="main" className={classes.root}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={6}
+          md={5}
+          className={classes.image}
+          style={{ display: "flex", justifyContent: "flex-start" }}
         >
-          <Typography
+          <div
             style={{
-              color: "white",
-              paddingTop: "30px",
-              fontFamily: "Andalus"
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+              height: "150px"
             }}
           >
-            Refugees & Mentors
-          </Typography>
-          <Typography
-            style={{
-              color: "white",
-              padding: "15px",
-              fontFamily: "Andalus",
-              marginLeft: "1rem"
-            }}
-          >
-            supports refugees and vulnerable migrants to improve their
-            employment prospects and get jobs
-          </Typography>
-        </div>
-      </Grid>
-      <Grid item xs={12} sm={8} md={6} component={Paper} elevation={6} square>
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Log in
-          </Typography>
-          <form className={classes.form} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="secondary"
-              className={classes.submit}
+            <Typography
+              style={{
+                color: "white",
+                paddingTop: "30px",
+                fontFamily: "Andalus"
+              }}
             >
-              Log In
-            </Button>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="secondary"
-              className={classes.submit}
+              Refugees & Mentors
+            </Typography>
+            <Typography
+              style={{
+                color: "white",
+                padding: "15px",
+                fontFamily: "Andalus",
+                marginLeft: "1rem"
+              }}
             >
-              Sign Up
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
+              supports refugees and vulnerable migrants to improve their
+              employment prospects and get jobs
+            </Typography>
+          </div>
+        </Grid>
+        <Grid item xs={12} sm={8} md={6} component={Paper} elevation={6} square>
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Log in
+            </Typography>
+            <form className={classes.form} noValidate>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                onChange={handleChange}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={handleChange}
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="secondary"
+                className={classes.submit}
+                onClick={signIn}
+              >
+                Log In
+              </Button>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="secondary"
+                className={classes.submit}
+              >
+                Sign Up
+              </Button>
+
+              {error ? <div>Wrong information. Try again</div> : null}
+
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
               </Grid>
-            </Grid>
-            <Box mt={5}></Box>
-          </form>
-        </div>
+              <Box mt={5}></Box>
+            </form>
+          </div>
+        </Grid>
       </Grid>
-    </Grid>
-  );
+    );
 }
 
 export default Login;
