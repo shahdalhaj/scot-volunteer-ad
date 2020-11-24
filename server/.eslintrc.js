f4eab74a063a8db992eea6347d0baf2b1a3b779d
teomrd <mironidis.gr@gmail.com>
2020-11-24 18:28:07 +0000

    disable line endings check

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,10 +1,13 @@
 module.exports = {
-  extends: ["@codeyourfuture/standard"],
-  parserOptions: {
-    ecmaVersion: 2017,
-  },
-  env: {
-    node: true,
-    es6: true,
-  },
+	extends: ["@codeyourfuture/standard"],
+	parserOptions: {
+		ecmaVersion: 2017,
+	},
+	env: {
+		node: true,
+		es6: true,
+	},
+	rules :{
+		"linebreak-style": ["error", "windows"],
+	},
 };
