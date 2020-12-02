de0ae37aa12e67c24f58c7262c49521962c9ee36
mhallaq <malhallaq@gmail.com>
2020-12-02 20:47:42 +0000

    replacing volunteer-topics with volunteer-blocks

--- server/db/recreate-schema.sql
+++ server/db/recreate-schema.sql
@@ -1,21 +1,21 @@
 -- Drop tables in case they already exist
 -- Drop tables in case they already exist
-DROP TABLE if exists users;
+DROP TABLE IF exists users;
 DROP TABLE IF EXISTS blocks CASCADE;
 DROP TABLE IF EXISTS topics CASCADE;
 DROP TABLE IF EXISTS questions CASCADE;
 DROP TABLE IF EXISTS volunteers CASCADE;
 DROP TABLE IF EXISTS volunteer_blocks CASCADE;
-DROP TABLE IF exists volunteer_answers CASCADE;
+DROP TABLE IF EXISTS volunteer_answers CASCADE;
 
 -- Create tables
--- public.users definition
+--  users definition
 
 -- Drop table
 
--- DROP TABLE public.users;
+-- DROP TABLE  users;
 
-CREATE TABLE public.users (
+CREATE TABLE  users (
 	id serial NOT NULL,
 	email varchar(200) NOT NULL,
 	"password" varchar(200) NOT NULL,
@@ -24,18 +24,18 @@ CREATE TABLE public.users (
 
 -- Permissions
 
-ALTER TABLE public.users OWNER TO app_user;
-GRANT ALL ON TABLE public.users TO app_user;
+-- ALTER TABLE  users OWNER TO app_user;
+-- GRANT ALL ON TABLE  users TO app_user;
 
 
 -----------------------------------------------------------------------------
--- public.blocks definition
+--  blocks definition
 
 -- Drop table
 
--- DROP TABLE public.blocks;
+-- DROP TABLE  blocks;
 
-CREATE TABLE public.blocks (
+CREATE TABLE  blocks (
 	block_id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
 	block_name varchar(255) NOT NULL,
 	CONSTRAINT blocks_pkey PRIMARY KEY (block_id)
@@ -43,17 +43,17 @@ CREATE TABLE public.blocks (
 
 -- Permissions
 
-ALTER TABLE public.blocks OWNER TO app_user;
-GRANT ALL ON TABLE public.blocks TO app_user;
+-- ALTER TABLE  blocks OWNER TO app_user;
+-- GRANT ALL ON TABLE  blocks TO app_user;
 
 
 
--- public.volunteers definition
+--  volunteers definition
 
 
--- DROP TABLE public.volunteers;
+-- DROP TABLE  volunteers;
 
-CREATE TABLE public.volunteers (
+CREATE TABLE  volunteers (
 	volunteer_id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
 	email varchar(255) NOT NULL,
 	CONSTRAINT volunteers_pkey PRIMARY KEY (volunteer_id)
@@ -61,16 +61,16 @@ CREATE TABLE public.volunteers (
 
 -- Permissions
 
-ALTER TABLE public.volunteers OWNER TO app_user;
-GRANT ALL ON TABLE public.volunteers TO app_user;
+-- ALTER TABLE  volunteers OWNER TO app_user;
+-- GRANT ALL ON TABLE  volunteers TO app_user;
 
 
--- public.topics definition
+--  topics definition
 
 
--- DROP TABLE public.topics;
+-- DROP TABLE  topics;
 
-CREATE TABLE public.topics (
+CREATE TABLE  topics (
 	topic_id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
 	topic_name varchar(255) NOT NULL,
 	document_name varchar(255) NOT NULL,
@@ -82,15 +82,15 @@ CREATE TABLE public.topics (
 
 -- Permissions
 
-ALTER TABLE public.topics OWNER TO app_user;
-GRANT ALL ON TABLE public.topics TO app_user;
+-- ALTER TABLE  topics OWNER TO app_user;
+-- GRANT ALL ON TABLE  topics TO app_user;
 
 
--- public."volunteer_Blocks" definition
+--  "volunteer_Blocks" definition
 
--- DROP TABLE public."volunteer_Blocks";
+-- DROP TABLE  "volunteer_Blocks";
 
-CREATE TABLE public.volunteer_blocks (
+CREATE TABLE  volunteer_blocks (
 	volunteer_block_id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
 	volunteer_id int4 NOT NULL,
 	assessment_start timestamp NULL,
@@ -103,16 +103,16 @@ CREATE TABLE public.volunteer_blocks (
 
 -- Permissions
 
-ALTER TABLE public."volunteer_Blocks" OWNER TO app_user;
-GRANT ALL ON TABLE public."volunteer_Blocks" TO app_user;
+-- ALTER TABLE  "volunteer_Blocks" OWNER TO app_user;
+-- GRANT ALL ON TABLE  "volunteer_Blocks" TO app_user;
 
 
--- public.questions definition
+--  questions definition
 
 
--- DROP TABLE public.questions;
+-- DROP TABLE  questions;
 
-CREATE TABLE public.questions (
+CREATE TABLE  questions (
 	question_id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
 	topic_id int4 NOT NULL,
 	question_text varchar(255) NOT NULL,
@@ -122,17 +122,17 @@ CREATE TABLE public.questions (
 
 -- Permissions
 
-ALTER TABLE public.questions OWNER TO app_user;
-GRANT ALL ON TABLE public.questions TO app_user;
+-- ALTER TABLE  questions OWNER TO app_user;
+-- GRANT ALL ON TABLE  questions TO app_user;
 
 
--- public.volunteer_answers definition
+--  volunteer_answers definition
 
 -- Drop table
 
--- DROP TABLE public.volunteer_answers;
+-- DROP TABLE  volunteer_answers;
 
-CREATE TABLE public.volunteer_answers (
+CREATE TABLE  volunteer_answers (
 	volunteer_block_id int4 NOT NULL,
 	question_id int4 NOT NULL,
 	answer varchar(255) NOT NULL,
@@ -143,8 +143,8 @@ CREATE TABLE public.volunteer_answers (
 
 -- Permissions
 
-ALTER TABLE public.volunteer_answers OWNER TO app_user;
-GRANT ALL ON TABLE public.volunteer_answers TO app_user;
+-- ALTER TABLE  volunteer_answers OWNER TO app_user;
+-- GRANT ALL ON TABLE  volunteer_answers TO app_user;
 
 
 
