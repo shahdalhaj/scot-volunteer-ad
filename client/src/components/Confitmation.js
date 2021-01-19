6ed2fd6be5b2b567c749f62319e38c86a3e8f39f
shahdalhaj <shooodyadoooly@gmail.com>
2021-01-19 21:15:00 +0000

    almost there

--- client/src/components/Confitmation.js
+++ client/src/components/Confitmation.js
@@ -1,6 +1,14 @@
-import { Typography } from "@material-ui/core";
-import React from "react";
+import { Typography, Button } from "@material-ui/core";
+import React, { useState } from "react";
 const confirmation = () => {
+  let [btnToggle, setBtnToggle] = useState(true);
+
+  const toggle = () => {
+    setBtnToggle(!btnToggle);
+  };
+  const redirect = () => {
+    window.location = "./assessment";
+  };
   return (
     <div
       style={{
@@ -13,12 +21,49 @@ const confirmation = () => {
         height: "68vh"
       }}
     >
-      <Typography>
-        Thank you for taking the time to complete all questions. We truly value
-        the information you have provided. Your responses will contribute to our
-        analyses and your assessment will be reviewd within the next 7 days.
-      </Typography>
-      <Typography>Refmentors Team</Typography>
+      {btnToggle && (
+        <div>
+          <Typography>
+            Thank you for compeleting the assessment click on the buttons down
+            below to retake the assessment or submit your answers
+          </Typography>
+          <div>
+            {" "}
+            <Button
+              style={{
+                backgroundColor: "#000000",
+                color: "white",
+                marginRight: 7
+              }}
+              onClick={toggle}
+            >
+              Submit
+            </Button>
+            <Button
+              style={{
+                backgroundColor: "#000000",
+                color: "white",
+                marginRight: 7
+              }}
+              onClick={redirect}
+            >
+              Retake
+            </Button>
+          </div>
+        </div>
+      )}
+      {!btnToggle && (
+        <div>
+          {" "}
+          <Typography>
+            Thank you for taking the time to complete all questions. We truly
+            value the information you have provided. Your responses will
+            contribute to our analyses and your assessment will be reviewd
+            within the next 3 working days.
+          </Typography>
+          <Typography>Refmentors Team</Typography>
+        </div>
+      )}
     </div>
   );
 };
